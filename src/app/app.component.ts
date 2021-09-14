746386be5ef0b53372c0748f9dc7403a62a7aec6
thiago662 <thiago1santos12@gmail.com>
2021-09-13 21:20:47 -0300

    projeto funcionando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,10 @@ import { WebSocketService } from './web-socket.service';
 export class AppComponent implements OnInit {
   users: any = [];
   user = {
-    name: ''
+    id: '',
+    name: '',
+    value: '',
+    selected: false
   };
 
   constructor(private webSocketService: WebSocketService) {}
@@ -18,12 +21,47 @@ export class AppComponent implements OnInit {
     this.newUser();
     this.myUser();
     this.allUsers();
+    this.userDeleted();
+    this.myDelete();
+    this.userUpdated();
+    this.myUpdate();
+    this.showAll();
+    this.showMe();
+    this.resetAll();
+    this.resetMe();
   }
 
+  // EMIT
   setUser() {
     this.webSocketService.emit('setUser', this.user);
   }
 
+  setValue() {
+    console.log('setValue', this.user);
+
+    this.webSocketService.emit('setValue', this.user);
+  }
+
+  delete() {
+    var user = JSON.parse(<string>localStorage.getItem('user'));
+    console.log('delete', user.id);
+
+    this.webSocketService.emit('delete', user.id);
+  }
+
+  show() {
+    console.log('show');
+
+    this.webSocketService.emit('show', []);
+  }
+
+  reset() {
+    console.log('reset');
+
+    this.webSocketService.emit('reset', []);
+  }
+
+  // LISTEN
   newUser() {
     this.webSocketService.listen('newUser').subscribe((data) => {
       this.users.push(data);
@@ -36,16 +74,95 @@ export class AppComponent implements OnInit {
       this.users.push(data);
       localStorage.setItem('user', JSON.stringify(data));
 
-      // var storedNames = JSON.parse(<string>localStorage.getItem('user'));
+      this.user = JSON.parse(<string>localStorage.getItem('user'));
       console.log('myUser', data);
     });
   }
 
-  delete() {
-    var user = JSON.parse(<string>localStorage.getItem('user'));
-    console.log(user.id);
+  userDeleted() {
+    this.webSocketService.listen('userDeleted').subscribe((data) => {
+      console.log('userDeleted', data);
+      this.users = this.users.filter(function(item: any) {
+          return item.id != data;
+      });
+    });
+  }
 
-    this.webSocketService.emit('delete', user.id);
+  myDelete() {
+    this.webSocketService.listen('myDelete').subscribe((data) => {
+      console.log('myDelete', data);
+      this.users = this.users.filter(function(item: any) {
+          return item.id != data;
+      });
+    });
+  }
+
+  userUpdated() {
+    this.webSocketService.listen('userUpdated').subscribe((data: any) => {
+      console.log('userUpdated', data);
+
+      var objIndex = this.users.findIndex(((obj: any) => obj.id == data));
+
+      this.users[objIndex].selected = true;
+    });
+  }
+
+  myUpdate() {
+    this.webSocketService.listen('myUpdate').subscribe((data: any) => {
+      console.log('myUpdate', data);
+
+      var objIndex = this.users.findIndex(((obj: any) => obj.id == data.id));
+
+      this.users[objIndex] = data;
+    });
+  }
+
+  showAll() {
+    this.webSocketService.listen('showAll').subscribe((data: any) => {
+      console.log('showAll', data);
+
+      for (const user of data) {
+        var objIndex = this.users.findIndex(((obj: any) => obj.id == user.id));
+
+        this.users[objIndex] = user;
+      }
+    });
+  }
+
+  showMe() {
+    this.webSocketService.listen('showMe').subscribe((data: any) => {
+      console.log('showMe', data);
+
+      for (const user of data) {
+        var objIndex = this.users.findIndex(((obj: any) => obj.id == user.id));
+
+        this.users[objIndex] = user;
+      }
+    });
+  }
+
+  resetMe() {
+    this.webSocketService.listen('resetMe').subscribe((data: any) => {
+      console.log('resetMe', data);
+
+      for (const user of data) {
+        var objIndex = this.users.findIndex(((obj: any) => obj.id == user.id));
+
+        this.users[objIndex] = user;
+      }
+    });
+  }
+
+  resetAll() {
+    this.webSocketService.listen('resetAll').subscribe((data: any) => {
+      console.log('resetAll', data);
+
+      for (const user of data) {
+        var objIndex = this.users.findIndex(((obj: any) => obj.id == user.id));
+
+        this.users[objIndex] = user;
+      }
+    });
   }
 
   allUsers() {
