1651b718416b78faa40aabe1483f111456d62059
thiago662 <thiago1santos12@gmail.com>
2021-09-22 22:12:26 -0300

    import env

--- src/app/web-socket.service.ts
+++ src/app/web-socket.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import {io} from 'socket.io-client';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebSocketService {
-
+  environment = environment;
   socket: any;
-  readonly url: string = 'ws://localhost:3000';
+  readonly url: string = environment.websocket;
 
   constructor() {
     this.socket = io(this.url);
